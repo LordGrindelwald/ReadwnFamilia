0248a7387be0759ab04d3010b5832cf8d5a76b01
Ian Grindelwald <164880317+LordGrindelwald@users.noreply.github.com>
2024-04-14 19:26:44 +0530

    Update FanNovel.lua

--- src/en/FanNovel.lua
+++ src/en/FanNovel.lua
@@ -90,7 +90,7 @@ local GENRES = {
 
 return Require("ReadWN")("https://www.fannovels.com", {
     id = 1308639969,
-    name = "FanNovel",
+    name = "FanNovel™",
     shrinkURLNovel = "^.-fannovels%.com",
     hasCloudFlare = true,
 
